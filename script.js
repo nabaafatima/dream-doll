b43dff0cbe2b30401ea564aadcc0c7a291971850
nabaafatima <nabaafatima7097@gmail.com>
2026-06-18 16:04:33 +0500

    added the logic for skin color

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const go = document.getElementById("letsgo");
 const main = document.getElementById("main");
 const text = document.getElementById("h1");
+const doll = document.getElementById("doll");
+const head = document.getElementById("head");
+const body = document.getElementById("body");
+const skincolor = document.getElementById("color");
 go.addEventListener("click", ()=>{
     main.style.opacity =  "1";
     go.style.opacity = "0";
     text.style.opacity = "0";
     go.style.pointerEvents = "none";
     text.styyle.pointerEvents = "none";
+})
+color.addEventListener('input', (e)=>{
+    head.style.backgroundColor = e.target.value;
+    body.style.backgroundColor = e.target.value;
 })
