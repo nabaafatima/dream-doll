15423c29d3ecd899f3efc97d71b3a494450a1d17
nabaafatima <nabaafatima7097@gmail.com>
2026-06-29 00:42:18 +0500

    h1 done

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const go = document.getElementById("letsgo");
 const main = document.getElementById("main");
+const arrow = document.getElementById("arrow")
 const text = document.getElementById("h1");
 const doll = document.getElementById("doll");
 const head = document.getElementById("head");
-const accessories = document.getElementById("accessories");
 const skincolor = document.getElementById("color");
 const hairs = document.getElementById("hair");
 const dresses = document.getElementById("clothes");
-const d1 = document.getElementById("dress");
-const d2 = document.getElementById("dress1");
+const d1 = document.getElementById("dress1");
+const d2 = document.getElementById("dress2");
 const d3 = document.getElementById("dress3");
 const d4 = document.getElementById("dress4");
 const d5 = document.getElementById("dress5");
@@ -25,12 +25,34 @@ go.addEventListener("click", ()=>{
     go.style.opacity = "0";
     text.style.opacity = "0";
     go.style.pointerEvents = "none";
-    text.styyle.pointerEvents = "none";
+    text.style.pointerEvents = "none";
 })
-color.addEventListener('input', (e)=>{
+skincolor.addEventListener('input', (e)=>{
     head.style.backgroundColor = e.target.value;
 })
 dresses.addEventListener("click",()=>{
     c = true;
+    h = false;
 })
-
+hairs.addEventListener("click", ()=>{
+    c = false;
+    h = true;
+})
+function s1(){
+    h1.style.opacity="1";
+    head.style.width = "90px";
+    head.style.height = "120px";
+    head.style.border.radius = "20px";
+    head.style.top = "100px";
+    head.style.left = "440px";
+}
+let currentIndex = 0;
+let darray = [d1,d2,d3,d4,d5];
+let harray = [h1,h2,h3,h4,h5];
+arrow.addEventListener("click", ()=>{
+    if(c===true) {
+        darray[currentIndex].style.opacity = "0";
+        currentIndex = (currentIndex + 1) % darray.length;
+        darray[currentIndex].style.opacity = "1";
+    };
+});
