bb8f2520faaf5e071815eb8309eae05a691d2514
nabaafatima <nabaafatima7097@gmail.com>
2026-06-28 23:42:56 +0500

    arranged all the dresses and added logic for dress changing

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const doll = document.getElementById("doll");
 const head = document.getElementById("head");
 const accessories = document.getElementById("accessories");
 const skincolor = document.getElementById("color");
+const hairs = document.getElementById("hair");
+const dresses = document.getElementById("clothes");
+const d1 = document.getElementById("dress");
+const d2 = document.getElementById("dress1");
+const d3 = document.getElementById("dress3");
+const d4 = document.getElementById("dress4");
+const d5 = document.getElementById("dress5");
+const h1 = document.getElementById("hair1");
+const h2 = document.getElementById("hair2");
+const h3 = document.getElementById("hair3");
+const h4 = document.getElementById("hair4");
+const h5 = document.getElementById("hair5");
 let a = false;
-let b = false;
+let h = false;
 let c = false;
 go.addEventListener("click", ()=>{
     main.style.opacity =  "1";
@@ -18,3 +30,7 @@ go.addEventListener("click", ()=>{
 color.addEventListener('input', (e)=>{
     head.style.backgroundColor = e.target.value;
 })
+dresses.addEventListener("click",()=>{
+    c = true;
+})
+
